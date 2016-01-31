f30b9f628271484285e29e3f0bda5b6e5c412222
JJW <jjwInNY@acd4130242937635.wireless.rit.edu>
2016-01-30 20:25:28 -0500

    New Hex from RGB

--- archived/camera_droplet-1/camera_droplet/ViewController.swift
+++ archived/camera_droplet-1/camera_droplet/ViewController.swift
@@ -6,10 +6,9 @@
 //  Copyright © 2016 Woof Warrior. All rights reserved.
 //
 
-import Foundation
-import UIKit
 
-extension UIImage{
+import UIKit
+extension UIImage {
     func getPixelColor(pos: CGPoint) -> UIColor {
         
         let pixelData = CGDataProviderCopyData(CGImageGetDataProvider(self.CGImage))
@@ -22,59 +21,9 @@ extension UIImage{
         let b = CGFloat(data[pixelInfo+2]) / CGFloat(255.0)
         let a = CGFloat(data[pixelInfo+3]) / CGFloat(255.0)
         
-        return UIColor(red: r, green: g, blue: b, alpha: a);
-        
+        return UIColor(red: r, green: g, blue: b, alpha: a)
     }
 }
-
-    extension UIColor{
-        func rgb() -> Int? {
-            var fRed : CGFloat = 0
-            var fGreen : CGFloat = 0
-            var fBlue : CGFloat = 0
-            var fAlpha: CGFloat = 0
-            if self.getRed(&fRed, green: &fGreen, blue: &fBlue, alpha: &fAlpha) {
-                let iRed = Int(fRed * 255.0)
-                let iGreen = Int(fGreen * 255.0)
-                let iBlue = Int(fBlue * 255.0)
-                let iAlpha = Int(fAlpha * 255.0)
-                
-                //  (Bits 24-31 are alpha, 16-23 are red, 8-15 are green, 0-7 are blue).
-                let rgb = (iAlpha << 24) + (iRed << 16) + (iGreen << 8) + iBlue
-                return rgb
-            } else {
-                // Could not extract RGBA components:
-                return 4
-        }
-        }
-        var rgbComponents:(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
-            var r:CGFloat = 0
-            var g:CGFloat = 0
-            var b:CGFloat = 0
-            var a:CGFloat = 0
-            if getRed(&r, green: &g, blue: &b, alpha: &a) {
-                return (r,g,b,a)
-            }
-            return (0,0,0,0)
-        }
-        // hue, saturation, brightness and alpha components from UIColor**
-        var hsbComponents:(hue: CGFloat, saturation: CGFloat, brightness: CGFloat, alpha: CGFloat) {
-            var hue:CGFloat = 0
-            var saturation:CGFloat = 0
-            var brightness:CGFloat = 0
-            var alpha:CGFloat = 0
-            if getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha){
-                return (hue,saturation,brightness,alpha)
-            }
-            return (0,0,0,0)
-        }
-        var htmlRGBColor:String {
-            return String(format: "#%02x%02x%02x", Int(rgbComponents.red * 255), Int(rgbComponents.green * 255),Int(rgbComponents.blue * 255))
-        }
-        var htmlRGBaColor:String {
-            return String(format: "#%02x%02x%02x%02x", Int(rgbComponents.red * 255), Int(rgbComponents.green * 255),Int(rgbComponents.blue * 255),Int(rgbComponents.alpha * 255) )
-        }
-    }
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     var imagePicker = UIImagePickerController()
@@ -83,7 +32,6 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     
     @IBOutlet weak var capture: UIButton!
     
-    @IBOutlet weak var colorText2: UILabel!
     @IBOutlet weak var setColor: UIButton!
     @IBOutlet weak var xval: UISlider!
     @IBOutlet weak var yval: UISlider!
@@ -121,8 +69,6 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         //Make sure point is within the image
         let color = image.getPixelColor(CGPointMake(CGFloat(xval.value), CGFloat(yval.value)))
         colorText.backgroundColor = color;
-        let myRedHueWebColor = String(color.htmlRGBColor);
-        colorText2.text = myRedHueWebColor;
     }
     
     
